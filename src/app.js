816e94fd8000d57606651bab83bb49f4dd0e42ba
shaunakkhare <work.khareshaunak@gmail.com>
2025-10-22 15:56:40 +0530

    Multiple Route handlers created

--- src/app.js
+++ src/app.js
@@ -2,13 +2,27 @@ const express = require('express')
 
 const app = express();
 
-app.use("/test", (req, res) => {
-    res.send("Hello World from test!");
-});
-
-app.use("/hello", (req, res) => {
-    res.send("Hello World from Home!");
-});
+app.get("/user", [(req, res, next) => {
+    console.log("hello this is the 1st request handler!.");
+    //res.send("This is 1st handler!");
+    next();
+},
+    (req, res, next) => {
+        console.log("This is the second request handler!");
+        //res.send("Hey there, Iam devTinder!");
+        next();
+    },
+    (req, res, next) => {
+        console.log("this is the 3rd handler");
+        //res.send("3rd handler response!");
+        next();
+    }],
+    (req, res, next) => {
+        console.log("this is the 4th handler");
+        res.send("3rd handler response!");
+        next();
+    }
+)
 
 
 app.listen(7777, () => {
